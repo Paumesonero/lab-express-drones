8b6994ba1b18d6ef2e7c68e885afdc8b1452ca12
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-06-10 14:50:28 +0200

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const droneSchema = new Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+
+})
+
+
+const droneExport = mongoose.model('Drone.model', droneSchema);
+module.exports = droneExport;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const droneRequired = require('../models/Drone.model');
+
+const drones = [
+    {
+        name: 'dji mini 2',
+        propellers: 4,
+        maxSpeed: 80
+    },
+    {
+        name: 'dji mavic 3',
+        propellers: 5,
+        maxSpeed: 110
+    },
+    {
+        name: 'dji fpv drone',
+        propellers: 4,
+        maxSpeed: 150
+    }
+];
+
+mongoose.connect("mongodb://localhost/lab-express-drones")
+    .then(x => console.log(`conected to {x.connection.name}`))
+    .then(() => {
+        return droneRequired.create(drones)
+    })
+    .catch(e => console.log(e))
+    .finally(() => {
+        mongoose.connection.close()
+    })
