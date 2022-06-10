2e20eb283e899312caa787957af9b71e6f4aacf9
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-06-10 15:17:54 +0200

    iteration 2 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const async = require('hbs/lib/async');
 const router = express.Router();
+const Drones = require('../models/Drone.model')
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  try {
+    const drones = await Drones.find({});
+    res.render('drones/list', { drones })
+  } catch (error) {
+    next(error)
+  }
+
 });
 
 router.get('/drones/create', (req, res, next) => {
